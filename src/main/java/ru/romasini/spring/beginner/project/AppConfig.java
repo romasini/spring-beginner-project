a95902069ad16a2086c8835506500baab25e19bf
Roman Sinitsyn <romasini@gmail.com>
2020-09-11 08:59:07 +0300

    Initial commit

---
+++ src/main/java/ru/romasini/spring/beginner/project/AppConfig.java
@@ -0,0 +1,10 @@
+package ru.romasini.spring.beginner.project;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ={"ru.romasini.spring.beginner.project"})
+public class AppConfig {
+
+}
---
+++ src/main/java/ru/romasini/spring/beginner/project/MainApp.java
@@ -0,0 +1,5 @@
+package ru.romasini.spring.beginner.project;
+
+public class MainApp {
+
+}

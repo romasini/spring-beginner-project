cdb5b9d2754eb5a1b1223938e3f92d16cbaf71dd
Roman Sinitsyn <romasini@gmail.com>
2020-09-11 21:42:03 +0300

    1.	Есть класс Product (id, название, цена). Товары хранятся в бине ProductRepository, в виде List<Product>, при старте в него нужно добавить 5 любых товаров. ProductRepository позволяет получить весь список или один товар по id. Создаем бин Cart, в который можно добавлять и удалять товары по id. Написать консольное приложение, позволяющее управлять корзиной. При каждом запросе корзины из контекста, должна создаваться новая корзина

--- src/main/java/ru/romasini/spring/beginner/project/MainApp.java
+++ src/main/java/ru/romasini/spring/beginner/project/MainApp.java
@@ -2,10 +2,90 @@ package ru.romasini.spring.beginner.project;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class MainApp {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ProductService productService = context.getBean("productService", ProductService.class);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Список команд:");
+            System.out.println("prod - список товаров");
+            System.out.println("cart - добавить корзину");
+            System.out.println("put - добавить товар в корзину");
+            System.out.println("del - удалить товар из корзины");
+            System.out.println("list - просмотреть список товаров в корзине");
+            System.out.println("exit - выход из программы");
+
+            String command = null;
+            Cart cart = null;
+            long temp = 0l;
+            while (true){
+                command = bufferedReader.readLine();
+
+                if(command.equals("exit")){
+                    break;
+                }
+
+                if(command.equals("prod")){
+                    List<Product> productList = productService.getProductList();
+                    if(productList.isEmpty()){
+                        System.out.println("Список товаров пуст");
+                    }
+
+                    StringBuilder str = new StringBuilder();
+                    for (Product p:productList) {
+                        str.append(p).append("\n");
+                    }
+                    System.out.println(str.toString());
+                }
+
+                if(command.equals("cart")){
+                    cart = context.getBean("cart", Cart.class);
+                    System.out.println("Корзина создана");
+                }
+
+                if(cart != null) {
+                    if (command.equals("list")) {
+                        System.out.println(cart);
+                    }
+
+                    if(command.equals("put")){
+                        System.out.println("Введите индекс товара");
+                        temp = Long.parseLong(bufferedReader.readLine());
+                        try {
+                            cart.putProduct(temp);
+                        }catch (RuntimeException e){
+                            System.out.println(e.getMessage());
+                        }
+
+                    }
+
+                    if(command.equals("del")){
+                        System.out.println("Введите индекс товара");
+                        temp = Long.parseLong(bufferedReader.readLine());
+                        try {
+                            cart.deleteProduct(temp);
+                        }catch (RuntimeException e){
+                            System.out.println(e.getMessage());
+                        }
+
+                    }
+                }
+
+            }
+
+            System.out.println("Программа завершена");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
         context.close();
     }
--- src/main/java/ru/romasini/spring/beginner/project/Product.java
+++ src/main/java/ru/romasini/spring/beginner/project/Product.java
@@ -1,11 +1,54 @@
 package ru.romasini.spring.beginner.project;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Product {
     private long id;
     private String name;
     private int cost;
 
+    public Product() {
+    }
+
+    public Product(long id, String name, int cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product " +
+                "индекс =" + id +
+                ", название ='" + name + '\'' +
+                ", цена =" + cost
+                ;
+    }
 }
